91f6b99456b09ec33867cc76f3ca910efdc1cf60
sepumarcela <yulimarce0616@gmail.com>
2026-06-19 12:04:37 -0500

    "Allow mobile access to technical sheets"

--- src/main/java/com/formas/cms/storage/TechnicalSheetController.java
+++ src/main/java/com/formas/cms/storage/TechnicalSheetController.java
@@ -9,12 +9,14 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/technical-sheets")
 public class TechnicalSheetController {
   private static final String TECHNICAL_SHEETS_FOLDER = "fichas-tecnicas";
