2cf8b8a72f616aeabdf092474a5e0515aaab59a7
sepumarcela <yulimarce0616@gmail.com>
2026-06-23 07:59:51 -0500

    "Corregir compilacion de pedidos"

--- src/main/java/com/formas/cms/orders/Order.java
+++ src/main/java/com/formas/cms/orders/Order.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
